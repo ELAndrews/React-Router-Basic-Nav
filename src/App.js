96d697105d1b3178e29c59829f7b33248bfef5e2
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2019-11-11 12:46:09 +0000

    Routes declared in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
-// import { Home, About, Contact, Navigation } from './components';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
@@ -9,6 +9,15 @@ import Navigation from './components/Navigation';
 const App = () => (
   <div>
     <Navigation />
+    <Route 
+    exact to="/"
+    component={Home} />
+    <Route 
+    exact to="/about"
+    component={About} />
+    <Route 
+    exact to="/contact"
+    component={Contact} />
   </div>
 );
 
