bce4b75a32f41dc5b5e7f53cbfe2b7b6dfe74352
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2019-11-11 12:22:39 +0000

    React Router implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+// import { Home, About, Contact, Navigation } from './components';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Navigation from './components/Navigation';
 
 const App = () => (
   <div>
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
 import Navigation from './Navigation';
+import App from '../App';
 
-export { Home, About, Contact, Navigation };
+
+// export { Home, About, Contact, Navigation };
+
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
